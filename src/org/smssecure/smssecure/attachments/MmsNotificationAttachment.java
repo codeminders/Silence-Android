signalapp/Signal-Android

Remove dedicated MMS download controls
// FREEBIE

src/org/thoughtcrime/securesms/attachments/MmsNotificationAttachment.java
@@ -1,0 +1,39 @@
+package org.thoughtcrime.securesms.attachments;
+
+
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+import org.thoughtcrime.securesms.database.AttachmentDatabase;
+import org.thoughtcrime.securesms.database.MmsDatabase;
+
+public class MmsNotificationAttachment extends Attachment {
+
+  public MmsNotificationAttachment(int status, long size) {
+    super("application/mms", getTransferStateFromStatus(status), size, null, null, null);
+  }
+
+  @Nullable
+  @Override
+  public Uri getDataUri() {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public Uri getThumbnailUri() {
+    return null;
+  }
+
+  private static int getTransferStateFromStatus(int status) {
+    if (status == MmsDatabase.Status.DOWNLOAD_INITIALIZED ||
+        status == MmsDatabase.Status.DOWNLOAD_NO_CONNECTIVITY)
+    {
+      return AttachmentDatabase.TRANSFER_PROGRESS_AUTO_PENDING;
+    } else if (status == MmsDatabase.Status.DOWNLOAD_CONNECTING) {
+      return AttachmentDatabase.TRANSFER_PROGRESS_STARTED;
+    } else {
+      return AttachmentDatabase.TRANSFER_PROGRESS_FAILED;
+    }
+  }
+}

src/org/thoughtcrime/securesms/database/MmsDatabase.java
@@ -34,6 +34,7 @@ import org.thoughtcrime.securesms.ApplicationContext;
 import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.attachments.Attachment;
 import org.thoughtcrime.securesms.attachments.DatabaseAttachment;
+import org.thoughtcrime.securesms.attachments.MmsNotificationAttachment;
 import org.thoughtcrime.securesms.crypto.AsymmetricMasterCipher;
 import org.thoughtcrime.securesms.crypto.MasterCipher;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
@@ -1075,28 +1076,6 @@ public class MmsDatabase extends MessagingDatabase {
     public static final int DOWNLOAD_SOFT_FAILURE    = 4;
     public static final int DOWNLOAD_HARD_FAILURE    = 5;
     public static final int DOWNLOAD_APN_UNAVAILABLE = 6;
-
-    public static boolean isDisplayDownloadButton(int status) {
-      return
-          status == DOWNLOAD_INITIALIZED     ||
-          status == DOWNLOAD_NO_CONNECTIVITY ||
-          status == DOWNLOAD_SOFT_FAILURE;
-    }
-
-    public static String getLabelForStatus(Context context, int status) {
-      switch (status) {
-        case DOWNLOAD_CONNECTING:      return context.getString(R.string.MmsDatabase_connecting_to_mms_server);
-        case DOWNLOAD_INITIALIZED:     return context.getString(R.string.MmsDatabase_downloading_mms);
-        case DOWNLOAD_HARD_FAILURE:    return context.getString(R.string.MmsDatabase_mms_download_failed);
-        case DOWNLOAD_APN_UNAVAILABLE: return context.getString(R.string.MmsDatabase_mms_pending_download);
-      }
-
-      return context.getString(R.string.MmsDatabase_downloading);
-    }
-
-    public static boolean isHardError(int status) {
-      return status == DOWNLOAD_HARD_FAILURE;
-    }
   }
 
   public class Reader {
@@ -1157,11 +1136,13 @@ public class MmsDatabase extends MessagingDatabase {
       if (!TextUtils.isEmpty(transactionId))
         transactionIdBytes = org.thoughtcrime.securesms.util.Util.toIsoBytes(transactionId);
 
+      SlideDeck slideDeck = new SlideDeck(context, new MmsNotificationAttachment(status, messageSize));
+
 
       return new NotificationMmsMessageRecord(context, id, recipients, recipients.getPrimaryRecipient(),
                                               addressDeviceId, dateSent, dateReceived, receiptCount, threadId,
                                               contentLocationBytes, messageSize, expiry, status,
-                                              transactionIdBytes, mailbox, subscriptionId);
+                                              transactionIdBytes, mailbox, subscriptionId, slideDeck);
     }
 
     private MediaMmsMessageRecord getMediaMmsMessageRecord(Cursor cursor) {

src/org/thoughtcrime/securesms/database/model/NotificationMmsMessageRecord.java
@@ -24,6 +24,7 @@ import org.thoughtcrime.securesms.database.SmsDatabase.Status;
 import org.thoughtcrime.securesms.database.MmsDatabase;
 import org.thoughtcrime.securesms.database.documents.NetworkFailure;
 import org.thoughtcrime.securesms.database.documents.IdentityKeyMismatch;
+import org.thoughtcrime.securesms.mms.SlideDeck;
 import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.recipients.Recipients;
 
@@ -37,12 +38,12 @@ import java.util.LinkedList;
  *
  */
 
-public class NotificationMmsMessageRecord extends MessageRecord {
+public class NotificationMmsMessageRecord extends MmsMessageRecord {
 
   private final byte[] contentLocation;
-  private final long messageSize;
-  private final long expiry;
-  private final int status;
+  private final long   messageSize;
+  private final long   expiry;
+  private final int    status;
   private final byte[] transactionId;
 
   public NotificationMmsMessageRecord(Context context, long id, Recipients recipients,
@@ -50,12 +51,12 @@ public class NotificationMmsMessageRecord extends MessageRecord {
                                       long dateSent, long dateReceived, int receiptCount,
                                       long threadId, byte[] contentLocation, long messageSize,
                                       long expiry, int status, byte[] transactionId, long mailbox,
-                                      int subscriptionId)
+                                      int subscriptionId, SlideDeck slideDeck)
   {
     super(context, id, new Body("", true), recipients, individualRecipient, recipientDeviceId,
           dateSent, dateReceived, threadId, Status.STATUS_NONE, receiptCount, mailbox,
           new LinkedList<IdentityKeyMismatch>(), new LinkedList<NetworkFailure>(), subscriptionId,
-          0, 0);
+          0, 0, slideDeck);
 
     this.contentLocation = contentLocation;
     this.messageSize     = messageSize;
@@ -90,11 +91,6 @@ public class NotificationMmsMessageRecord extends MessageRecord {
   }
 
   @Override
-  public boolean isFailed() {
-    return MmsDatabase.Status.isHardError(status);
-  }
-
-  @Override
   public boolean isSecure() {
     return false;
   }
@@ -102,11 +98,6 @@ public class NotificationMmsMessageRecord extends MessageRecord {
   @Override
   public boolean isPending() {
     return false;
-  }
-
-  @Override
-  public boolean isMms() {
-    return true;
   }
 
   @Override
@@ -121,6 +112,12 @@ public class NotificationMmsMessageRecord extends MessageRecord {
 
   @Override
   public SpannableString getDisplayBody() {
-    return emphasisAdded(context.getString(R.string.NotificationMmsMessageRecord_multimedia_message));
+    if (status == MmsDatabase.Status.DOWNLOAD_INITIALIZED) {
+      return emphasisAdded(context.getString(R.string.NotificationMmsMessageRecord_multimedia_message));
+    } else if (status == MmsDatabase.Status.DOWNLOAD_CONNECTING) {
+      return emphasisAdded(context.getString(R.string.NotificationMmsMessageRecord_downloading_mms_message));
+    } else {
+      return emphasisAdded(context.getString(R.string.NotificationMmsMessageRecord_error_downloading_mms_message));
+    }
   }
 }

src/org/thoughtcrime/securesms/mms/MmsSlide.java
@@ -1,0 +1,21 @@
+package org.thoughtcrime.securesms.mms;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import org.thoughtcrime.securesms.attachments.Attachment;
+
+public class MmsSlide extends ImageSlide {
+
+  public MmsSlide(@NonNull Context context, @NonNull Attachment attachment) {
+    super(context, attachment);
+  }
+
+  @NonNull
+  @Override
+  public String getContentDescription() {
+    return "MMS";
+  }
+
+}

src/org/thoughtcrime/securesms/util/MediaUtil.java
@@ -18,6 +18,7 @@ import org.thoughtcrime.securesms.mms.AudioSlide;
 import org.thoughtcrime.securesms.mms.DecryptableStreamUriLoader.DecryptableUri;
 import org.thoughtcrime.securesms.mms.GifSlide;
 import org.thoughtcrime.securesms.mms.ImageSlide;
+import org.thoughtcrime.securesms.mms.MmsSlide;
 import org.thoughtcrime.securesms.mms.PartAuthority;
 import org.thoughtcrime.securesms.mms.Slide;
 import org.thoughtcrime.securesms.mms.VideoSlide;
@@ -79,6 +80,8 @@ public class MediaUtil {
       slide = new VideoSlide(context, attachment);
     } else if (ContentType.isAudioType(attachment.getContentType())) {
       slide = new AudioSlide(context, attachment);
+    } else if (isMms(attachment.getContentType())) {
+      slide = new MmsSlide(context, attachment);
     }
 
     return slide;
@@ -126,6 +129,10 @@ public class MediaUtil {
     in.close();
 
     return size;
+  }
+
+  public static boolean isMms(String contentType) {
+    return !TextUtils.isEmpty(contentType) && contentType.trim().equals("application/mms");
   }
 
   public static boolean isGif(String contentType) {

codeminders/Silence-Android

Upstream commit: https://github.com/signalapp/Signal-Android/commit/235a8472d985498ade58e57c957f8f883348578e

src/org/smssecure/smssecure/attachments/MmsNotificationAttachment.java
@@ -1,0 +1,39 @@
+package org.smssecure.smssecure.attachments;
+
+
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+import org.smssecure.smssecure.database.AttachmentDatabase;
+import org.smssecure.smssecure.database.MmsDatabase;
+
+public class MmsNotificationAttachment extends Attachment {
+
+  public MmsNotificationAttachment(int status, long size) {
+    super("application/mms", getTransferStateFromStatus(status), size, null, null, null);
+  }
+
+  @Nullable
+  @Override
+  public Uri getDataUri() {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public Uri getThumbnailUri() {
+    return null;
+  }
+
+  private static int getTransferStateFromStatus(int status) {
+    if (status == MmsDatabase.Status.DOWNLOAD_INITIALIZED ||
+        status == MmsDatabase.Status.DOWNLOAD_NO_CONNECTIVITY)
+    {
+      return AttachmentDatabase.TRANSFER_PROGRESS_AUTO_PENDING;
+    } else if (status == MmsDatabase.Status.DOWNLOAD_CONNECTING) {
+      return AttachmentDatabase.TRANSFER_PROGRESS_STARTED;
+    } else {
+      return AttachmentDatabase.TRANSFER_PROGRESS_FAILED;
+    }
+  }
+}

src/org/smssecure/smssecure/database/MmsDatabase.java
@@ -37,6 +37,7 @@ import org.smssecure.smssecure.ApplicationContext;
 import org.smssecure.smssecure.R;
 import org.smssecure.smssecure.attachments.Attachment;
 import org.smssecure.smssecure.attachments.DatabaseAttachment;
+import org.smssecure.smssecure.attachments.MmsNotificationAttachment;
 import org.smssecure.smssecure.crypto.MasterCipher;
 import org.smssecure.smssecure.crypto.MasterSecret;
 import org.smssecure.smssecure.database.documents.IdentityKeyMismatch;
@@ -829,29 +830,6 @@ public class MmsDatabase extends MessagingDatabase {
     public static final int DOWNLOAD_SOFT_FAILURE    = 4;
     public static final int DOWNLOAD_HARD_FAILURE    = 5;
     public static final int DOWNLOAD_APN_UNAVAILABLE = 6;
-
-    public static boolean isDisplayDownloadButton(Context context, int status) {
-      return
-          status == DOWNLOAD_INITIALIZED     ||
-          status == DOWNLOAD_NO_CONNECTIVITY ||
-          status == DOWNLOAD_SOFT_FAILURE    ||
-         (status == DOWNLOAD_APN_UNAVAILABLE && SilencePreferences.seenManualMmsSettings(context));
-    }
-
-    public static String getLabelForStatus(Context context, int status) {
-      switch (status) {
-        case DOWNLOAD_CONNECTING:      return context.getString(R.string.MmsDatabase_connecting_to_mms_server);
-        case DOWNLOAD_INITIALIZED:     return context.getString(R.string.MmsDatabase_downloading_mms);
-        case DOWNLOAD_HARD_FAILURE:    return context.getString(R.string.MmsDatabase_mms_download_failed);
-        case DOWNLOAD_APN_UNAVAILABLE: return context.getString(R.string.MmsDatabase_mms_pending_download);
-      }
-
-      return context.getString(R.string.MmsDatabase_downloading);
-    }
-
-    public static boolean isHardError(int status) {
-      return status == DOWNLOAD_HARD_FAILURE;
-    }
   }
 
   public static class MmsNotificationInfo {
@@ -936,11 +914,12 @@ public class MmsDatabase extends MessagingDatabase {
       if (!TextUtils.isEmpty(transactionId))
         transactionIdBytes = org.smssecure.smssecure.util.Util.toIsoBytes(transactionId);
 
+      SlideDeck slideDeck = new SlideDeck(context, new MmsNotificationAttachment(status, messageSize));
 
       return new NotificationMmsMessageRecord(context, id, recipients, recipients.getPrimaryRecipient(),
                                               addressDeviceId, dateSent, dateReceived, dateDeliveryReceived, threadId,
                                               contentLocationBytes, messageSize, expiry, status,
-                                              transactionIdBytes, mailbox, subscriptionId);
+                                              transactionIdBytes, mailbox, subscriptionId, slideDeck);
     }
 
     private MediaMmsMessageRecord getMediaMmsMessageRecord(Cursor cursor) {

src/org/smssecure/smssecure/database/model/NotificationMmsMessageRecord.java
@@ -24,6 +24,7 @@ import org.smssecure.smssecure.database.SmsDatabase.Status;
 import org.smssecure.smssecure.database.MmsDatabase;
 import org.smssecure.smssecure.database.documents.NetworkFailure;
 import org.smssecure.smssecure.database.documents.IdentityKeyMismatch;
+import org.smssecure.smssecure.mms.SlideDeck;
 import org.smssecure.smssecure.recipients.Recipient;
 import org.smssecure.smssecure.recipients.Recipients;
 
@@ -37,12 +38,12 @@ import java.util.LinkedList;
  *
  */
 
-public class NotificationMmsMessageRecord extends MessageRecord {
+public class NotificationMmsMessageRecord extends MmsMessageRecord {
 
   private final byte[] contentLocation;
-  private final long messageSize;
-  private final long expiry;
-  private final int status;
+  private final long   messageSize;
+  private final long   expiry;
+  private final int    status;
   private final byte[] transactionId;
 
   public NotificationMmsMessageRecord(Context context, long id, Recipients recipients,
@@ -50,11 +51,12 @@ public class NotificationMmsMessageRecord extends MessageRecord {
                                       long dateSent, long dateReceived, long dateDeliveryReceived,
                                       long threadId, byte[] contentLocation, long messageSize,
                                       long expiry, int status, byte[] transactionId, long mailbox,
-                                      int subscriptionId)
+                                      int subscriptionId, SlideDeck slideDeck)
   {
     super(context, id, new Body("", true), recipients, individualRecipient, recipientDeviceId,
           dateSent, dateReceived, threadId, Status.STATUS_NONE, dateDeliveryReceived, mailbox,
-          new LinkedList<IdentityKeyMismatch>(), new LinkedList<NetworkFailure>(), subscriptionId);
+          new LinkedList<IdentityKeyMismatch>(), new LinkedList<NetworkFailure>(), subscriptionId,
+          slideDeck);
 
     this.contentLocation = contentLocation;
     this.messageSize     = messageSize;
@@ -89,11 +91,6 @@ public class NotificationMmsMessageRecord extends MessageRecord {
   }
 
   @Override
-  public boolean isFailed() {
-    return MmsDatabase.Status.isHardError(status);
-  }
-
-  @Override
   public boolean isSecure() {
     return false;
   }
@@ -104,17 +101,23 @@ public class NotificationMmsMessageRecord extends MessageRecord {
   }
 
   @Override
-  public boolean isMms() {
-    return true;
-  }
-
-  @Override
   public boolean isMmsNotification() {
     return true;
   }
 
   @Override
+  public boolean isMediaPending() {
+    return true;
+  }
+
+  @Override
   public SpannableString getDisplayBody() {
-    return emphasisAdded(context.getString(R.string.NotificationMmsMessageRecord_multimedia_message));
+    if (status == MmsDatabase.Status.DOWNLOAD_INITIALIZED) {
+      return emphasisAdded(context.getString(R.string.NotificationMmsMessageRecord_multimedia_message));
+    } else if (status == MmsDatabase.Status.DOWNLOAD_CONNECTING) {
+      return emphasisAdded(context.getString(R.string.NotificationMmsMessageRecord_downloading_mms_message));
+    } else {
+      return emphasisAdded(context.getString(R.string.NotificationMmsMessageRecord_error_downloading_mms_message));
+    }
   }
 }

src/org/smssecure/smssecure/mms/MmsSlide.java
@@ -1,0 +1,20 @@
+package org.smssecure.smssecure.mms;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import org.smssecure.smssecure.attachments.Attachment;
+
+public class MmsSlide extends ImageSlide {
+
+  public MmsSlide(@NonNull Context context, @NonNull Attachment attachment) {
+    super(context, attachment);
+  }
+
+  @NonNull
+  @Override
+  public String getContentDescription() {
+    return "MMS";
+  }
+
+}

src/org/smssecure/smssecure/util/MediaUtil.java
@@ -18,6 +18,7 @@ import org.smssecure.smssecure.mms.AudioSlide;
 import org.smssecure.smssecure.mms.DecryptableStreamUriLoader.DecryptableUri;
 import org.smssecure.smssecure.mms.GifSlide;
 import org.smssecure.smssecure.mms.ImageSlide;
+import org.smssecure.smssecure.mms.MmsSlide;
 import org.smssecure.smssecure.mms.PartAuthority;
 import org.smssecure.smssecure.mms.Slide;
 import org.smssecure.smssecure.mms.VideoSlide;
@@ -83,6 +84,8 @@ public class MediaUtil {
       slide = new VideoSlide(context, attachment);
     } else if (isAudioType(attachment.getContentType())) {
       slide = new AudioSlide(context, attachment);
+    } else if (isMms(attachment.getContentType())) {
+      slide = new MmsSlide(context, attachment);
     }
 
     return slide;
@@ -146,6 +149,10 @@ public class MediaUtil {
 
   public static boolean isVideo(Attachment attachment) {
     return isVideoType(attachment.getContentType());
+  }
+
+  public static boolean isMms(String contentType) {
+    return !TextUtils.isEmpty(contentType) && contentType.trim().equals("application/mms");
   }
 
   public static boolean isGif(String contentType) {