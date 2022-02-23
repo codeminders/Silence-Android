SamSprung/Signal-Android

use ConfirmIdentityDialog instead of ReceiveKeyDialog
also switch to AppCompatDialog

src/org/thoughtcrime/securesms/ConversationItem.java
@@ -43,6 +43,7 @@ import org.thoughtcrime.securesms.database.DatabaseFactory;
 import org.thoughtcrime.securesms.database.MmsDatabase;
 import org.thoughtcrime.securesms.database.MmsSmsDatabase;
 import org.thoughtcrime.securesms.database.SmsDatabase;
+import org.thoughtcrime.securesms.database.documents.IdentityKeyMismatch;
 import org.thoughtcrime.securesms.database.model.MediaMmsMessageRecord;
 import org.thoughtcrime.securesms.database.model.MessageRecord;
 import org.thoughtcrime.securesms.database.model.NotificationMmsMessageRecord;
@@ -369,10 +370,21 @@ public class ConversationItem extends LinearLayout {
     contactPhoto.setVisibility(View.VISIBLE);
   }
 
+  private IdentityKeyMismatch getKeyMismatch(final MessageRecord record) {
+    if (record.isIdentityMismatchFailure()) {
+      for (final IdentityKeyMismatch mismatch : record.getIdentityKeyMismatches()) {
+        if (mismatch.getRecipientId() == record.getIndividualRecipient().getRecipientId()) {
+          return mismatch;
+        }
+      }
+    }
+    return null;
+  }
+
   /// Event handlers
 
   private void handleKeyExchangeClicked() {
-    ReceiveKeyDialog.build(context, masterSecret, messageRecord).show();
+    new ConfirmIdentityDialog(context, masterSecret, messageRecord, getKeyMismatch(messageRecord)).show();
   }
 
   private class ThumbnailClickListener implements ThumbnailView.ThumbnailClickListener {

codeminders/Silence-Android

Upstream commit: https://github.com/signalapp/Signal-Android/commit/72bd6d5844590f7087f36fafaf582acfa63a5f3f

src/org/smssecure/smssecure/ConversationItem.java
@@ -53,6 +53,7 @@ import org.smssecure.smssecure.database.DatabaseFactory;
 import org.smssecure.smssecure.database.MmsDatabase;
 import org.smssecure.smssecure.database.MmsSmsDatabase;
 import org.smssecure.smssecure.database.SmsDatabase;
+import org.smssecure.smssecure.database.documents.IdentityKeyMismatch;
 import org.smssecure.smssecure.database.model.MediaMmsMessageRecord;
 import org.smssecure.smssecure.database.model.MessageRecord;
 import org.smssecure.smssecure.database.model.NotificationMmsMessageRecord;
@@ -533,10 +534,23 @@ public class ConversationItem extends LinearLayout
     contactPhoto.setVisibility(View.VISIBLE);
   }
 
+  private IdentityKeyMismatch getKeyMismatch(final MessageRecord record) {
+    if (record.isIdentityMismatchFailure()) {
+      Log.w(TAG, "isIdentityMismatchFailure(): true");
+      for (final IdentityKeyMismatch mismatch : record.getIdentityKeyMismatches()) {
+        if (mismatch.getRecipientId() == record.getIndividualRecipient().getRecipientId()) {
+          return mismatch;
+        }
+      }
+    }
+    Log.w(TAG, "Returning null IdentityKeyMismatch...");
+    return null;
+  }
+
   /// Event handlers
 
   private void handleKeyExchangeClicked() {
-    new ReceiveKeyDialog(context, masterSecret, messageRecord).show();
+    new ConfirmIdentityDialog(context, masterSecret, messageRecord, getKeyMismatch(messageRecord)).show();
   }
 
   private void handleLegacyKeyExchangeClicked() {